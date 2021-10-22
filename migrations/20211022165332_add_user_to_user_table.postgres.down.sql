f1809ddb69d8498f61ba5fe930ccf623b6c14989
Ernest Addae <earnestaddae@gmail.com>
2021-10-23 00:57:48 +0800

    Add default admin user to app

---
+++ migrations/20211022165332_add_user_to_user_table.postgres.down.sql
@@ -0,0 +1,1 @@
+delete from users where email = 'earnest@admin.com';
---
+++ migrations/20211022165332_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'earnest',E'addae',E'earnest@admin.com',E'$2a$12$yugGbnUdFZo0vSm1QLDuNO0thWS7q57I.sjgR.GOK1z/mHIQk.Nii',3,E'2021-10-23 00:00:00',E'2021-10-23 00:00:00');
